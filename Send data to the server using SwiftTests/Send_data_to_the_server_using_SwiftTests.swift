0255544ba676a2645e854f0f3dcfeb3fa5c97725
Avindiamanta <avinophotography@gmail.com>
2016-03-18 16:55:24 +0700

    Initial Commit

---
+++ Send data to the server using SwiftTests/Send_data_to_the_server_using_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Send_data_to_the_server_using_SwiftTests.swift
+//  Send data to the server using SwiftTests
+//
+//  Created by Tiffany Diamanta on 3/18/16.
+//  Copyright © 2016 Tiffany Diamanta. All rights reserved.
+//
+
+import XCTest
+@testable import Send_data_to_the_server_using_Swift
+
+class Send_data_to_the_server_using_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
